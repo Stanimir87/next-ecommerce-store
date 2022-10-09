267159e155303eabf1b98495c164c4cc0ec69a00
Stanimir87 <103177983+Stanimir87@users.noreply.github.com>
2022-10-09 11:52:30 +0300

    Added MUI theme

--- components/Layout.js
+++ components/Layout.js
@@ -6,41 +6,70 @@ import {
   Typography,
   Container,
   Link,
+  ThemeProvider,
+  CssBaseline,
 } from "@material-ui/core";
+import { createTheme } from "@material-ui/core/styles";
 import useStyles from "../utils/styles";
 import NextLink from "next/link";
 
-export default function Layout({description, title, children }) {
+export default function Layout({ description, title, children }) {
+  const theme = createTheme({
+    typography: {
+      h1: {
+        fontSize: "1.6rem",
+        fontWeight: 400,
+        margin: "1rem 0",
+      },
+      h2: {
+        fontSize: "1.4rem",
+        fontWeight: 400,
+        margin: "1rem 0",
+      },
+    },
+    palette: {
+      type: "light",
+      primary: {
+        main: "#f0c000",
+      },
+      secondary: {
+        main: "#208080",
+      },
+    },
+  });
   const classes = useStyles();
 
   return (
     <div>
       <Head>
-        <title>{title ? `${title} - Marketplace` : 'Marketplace'}</title>
+        <title>{title ? `${title} - Marketplace` : "Marketplace"}</title>
         {description && <meta name="description" content={description}></meta>}
       </Head>
-      <AppBar position="static" className={classes.navbar}>
-        <Toolbar>
-          <NextLink href="/" passHref>
-            <Link>
-              <Typography className={classes.brand}>Marketplace</Typography>
-            </Link>
-          </NextLink>
-          <div className={classes.grow}></div>
-          <div>
-            <NextLink href="/cart"  passHref>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <AppBar position="static" className={classes.navbar}>
+          <Toolbar>
+            <NextLink href="/" passHref>
+              <Link>
+                <Typography className={classes.brand}>Marketplace</Typography>
+              </Link>
+            </NextLink>
+            <div className={classes.grow}></div>
+            <div>
+              <NextLink href="/cart" passHref>
                 <Link>Cart</Link>
-            </NextLink>
-            <NextLink href="/login"  passHref>
+              </NextLink>
+              <NextLink href="/login" passHref>
                 <Link>Login</Link>
-            </NextLink>
-          </div>
-        </Toolbar>
-      </AppBar>
-      <Container className={classes.main}>{children}</Container>
-      <footer className={classes.footer}>
-        <Typography>Powered by Next, MUI, MongoDb.</Typography>
-      </footer>
+              </NextLink>
+            </div>
+          </Toolbar>
+        </AppBar>
+        <Container className={classes.main}>{children}</Container>
+        <footer className={classes.footer}>
+          <Typography>Powered by Next, MUI, MongoDb.</Typography>
+        </footer>
+      </ThemeProvider>
     </div>
   );
 }
