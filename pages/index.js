b029e511d66e680f30906f9f2fc2c54d20242445
Stanimir87 <103177983+Stanimir87@users.noreply.github.com>
2022-10-17 17:08:02 +0300

    Added product API, data loads from DB

--- pages/index.js
+++ pages/index.js
@@ -9,29 +9,32 @@ import {
   CardActions,
   Button,
 } from "@material-ui/core";
-import NextLink from 'next/link';
+import NextLink from "next/link";
 import data from "../utils/data";
+import db from "../utils/db";
+import Product from "../models/Product";
 
-export default function Home() {
+export default function Home(props) {
+  const { products } = props;
   return (
     <Layout>
       <div>
         <h1>Products</h1>
         <Grid container spacing={3}>
-          {data.products.map((product) => (
+          {products.map((product) => (
             <Grid item md={4} key={product.name}>
               <Card>
                 <NextLink href={`/product/${product.slug}`} passHref>
-                <CardActionArea>
-                  <CardMedia
-                    component="img"
-                    image={product.image}
-                    title={product.name}
-                  />
-                  <CardContent>
-                    <Typography>{product.name}</Typography>
-                  </CardContent>
-                </CardActionArea>
+                  <CardActionArea>
+                    <CardMedia
+                      component="img"
+                      image={product.image}
+                      title={product.name}
+                    />
+                    <CardContent>
+                      <Typography>{product.name}</Typography>
+                    </CardContent>
+                  </CardActionArea>
                 </NextLink>
                 <CardActions>
                   <Typography>BGN {product.price}</Typography>
@@ -46,4 +49,16 @@ export default function Home() {
       </div>
     </Layout>
   );
+}
+
+export async function getServerSideProps() {
+  await db.connect();
+  const products = await Product.find({}).lean();
+  await db.disconnect();
+  console.log(products);
+  return {
+    props: {
+      products: products.map(db.convertDocToObj),
+    },
+  };
 }
--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -14,12 +14,12 @@ import Layout from "../../components/Layout";
 import data from "../../utils/data";
 import useStyles from "../../utils/styles";
 import Image from "next/image";
+import Product from "../../models/Product";
+import db from "../../utils/db";
 
-export default function ProductScreen() {
+export default function ProductScreen(props) {
+  const {product} = props;
   const classes = useStyles();
-  const router = useRouter();
-  const { slug } = router.query;
-  const product = data.products.find((a) => a.slug === slug);
   if (!product) {
     return <div>Product not found.</div>;
   }
@@ -89,4 +89,19 @@ export default function ProductScreen() {
       </Grid>
     </Layout>
   );
+}
+
+export async function getServerSideProps(context) {
+  const {params} = context;
+  const {slug} = params;
+
+  await db.connect();
+  const product = await Product.findOne({slug}).lean();
+  await db.disconnect();
+  console.log(product);
+  return {
+    props: {
+      product: db.convertDocToObj(product),
+    },
+  };
 }
